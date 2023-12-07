893991ed6249f1dfba655fd1f6237131317b24be
entremotivator <104661532+entremotivator@users.noreply.github.com>
2023-12-07 00:55:36 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,30 +1,17 @@
-import streamlit as st
 import google.generativeai as palm
-import pprint
 import time
-from trulens_eval import Tru, TruCustomApp, Feedback, Select
-from trulens_eval.feedback import Groundedness
-from trulens_eval.feedback.provider.openai import OpenAI as fOpenAI
-import numpy as np
-from dotenv import load_dotenv
+import streamlit as st
 
-# Trulens setup
-tru = Tru()
-load_dotenv()
-
-# Existing code for Old English Teacher Chat
-
-# Configure Palm
-palm.configure(api_key=st.secrets.get("GENERATIVE_AI_API_KEY"))
-
+st.header('Old English Teacher Chat')
+palm.configure(api_key= st.secrets["GENERATIVE_AI_API_KEY"])
 if "model" not in st.session_state:
     st.session_state["model"] = "models/chat-bison-001"
 
-context = "You're a native Anglo-Saxon, once living in ancient England. you're teleported to the present era of modern United Kingdom. You're now teaching Old English to the present modern era English speaking."
+context = "You're a native Anglo-Saxon, once living in ancient England. you're teleported to the present era of modern United Kingdom. You're now teaching Old English to the present modern era english speaking."
 
 if "messages" not in st.session_state:
     st.session_state.messages = []
-
+    
 for message in st.session_state.messages:
     with st.chat_message(message["role"]):
         st.markdown(message["content"])
@@ -37,7 +24,7 @@ if prompt := st.chat_input("What is up?"):
     with st.chat_message("user"):
         st.markdown(prompt)
 
-    # Display assistant response in chat message container
+# Display assistant response in chat message container
     with st.chat_message("teacher"):
         message_placeholder = st.empty()
         full_response = ""
@@ -58,37 +45,3 @@ if prompt := st.chat_input("What is up?"):
         message_placeholder.markdown(full_response)
     # Add assistant response to chat history
     st.session_state.messages.append({"role": "teacher", "content": full_response})
-
-# Trulens integration
-class RAG_from_scratch:
-    def __init__(self):
-        self.fopenai = fOpenAI()  # Instantiate fopenai here
-        self.grounded = Groundedness(groundedness_provider=self.fopenai)
-        self.f_groundedness = (
-            Feedback(self.grounded.groundedness_measure_with_cot_reasons, name="Groundedness")
-            .on(Select.RecordCalls.retrieve.rets.collect())
-            .on_output()
-            .aggregate(self.grounded.grounded_statements_aggregator)
-        )
-
-        self.f_qa_relevance = (
-            Feedback(self.fopenai.relevance_with_cot_reasons, name="Answer Relevance")
-            .on(Select.RecordCalls.retrieve.args.query)
-            .on_output()
-        )
-
-        self.f_context_relevance = (
-            Feedback(self.fopenai.qs_relevance_with_cot_reasons, name="Context Relevance")
-            .on(Select.RecordCalls.retrieve.args.query)
-            .on(Select.RecordCalls.retrieve.rets.collect())
-            .aggregate(np.mean)
-        )
-
-rag_instance = RAG_from_scratch()
-
-# Make sure to adjust this query based on your specific use case
-rag_instance.grounded.query("When was the University of Washington founded?")
-
-# Continue with the rest of your Streamlit app code
-st.write(tru.get_leaderboard(app_ids=["RAG v1"]))
-tru.run_dashboard()
