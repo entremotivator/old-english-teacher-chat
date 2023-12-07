6f9906297d65f4bab2f43d78a6e23f2230876d5d
entremotivator <104661532+entremotivator@users.noreply.github.com>
2023-12-07 03:57:49 -0500

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import streamlit as st
 import time
 import google.generativeai as palm
 import trulens_eval
+import os
 from trulens_eval import Tru, TruCustomApp, Feedback, Select
 from trulens_eval.feedback import Groundedness
 from trulens_eval.feedback.provider.openai import OpenAI as fOpenAI
