34c4cf2a10b527a29a7b1e3db57db6dde2afa7c2
entremotivator <104661532+entremotivator@users.noreply.github.com>
2023-12-07 01:22:53 -0500

    Update app.py

--- app.py
+++ app.py
@@ -3,45 +3,41 @@ import time
 import streamlit as st
 
 st.header('Old English Teacher Chat')
-palm.configure(api_key= st.secrets["GENERATIVE_AI_API_KEY"])
+palm.configure(api_key=st.secrets["GENERATIVE_AI_API_KEY"])
+
 if "model" not in st.session_state:
     st.session_state["model"] = "models/chat-bison-001"
 
-context = "You're a native Anglo-Saxon, once living in ancient England. you're teleported to the present era of modern United Kingdom. You're now teaching Old English to the present modern era english speaking."
+context = "You're a native Anglo-Saxon, once living in ancient England. you're teleported to the present era of modern United Kingdom. You're now teaching Old English to the present modern era English speaking."
 
 if "messages" not in st.session_state:
     st.session_state.messages = []
-    
+
 for message in st.session_state.messages:
-    with st.chat_message(message["role"]):
-        st.markdown(message["content"])
+    st.text(f'{message["role"]}: {message["content"]}')
 
 # Accept user input
-if prompt := st.chat_input("What is up?"):
+prompt = st.text_input("What is up?")
+
+if prompt:
     # Add user message to chat history
     st.session_state.messages.append({"role": "user", "content": prompt})
-    # Display user message in chat message container
-    with st.chat_message("user"):
-        st.markdown(prompt)
 
-# Display assistant response in chat message container
-    with st.chat_message("teacher"):
-        message_placeholder = st.empty()
-        full_response = ""
-        teacher_response = palm.chat(
-            context=context,
-            messages=prompt,
-            temperature=0.25,
-            candidate_count=1,
-            top_k=40,
-            top_p=0.95,
-        ).last
-        # Simulate stream of response with milliseconds delay
-        for chunk in teacher_response.split():
-            full_response += chunk + " "
-            time.sleep(0.05)
-            # Add a blinking cursor to simulate typing
-            message_placeholder.markdown(full_response + "▌")
-        message_placeholder.markdown(full_response)
-    # Add assistant response to chat history
-    st.session_state.messages.append({"role": "teacher", "content": full_response})
+    # Display assistant response
+    st.text("Teacher: (thinking...)")
+
+    teacher_response = palm.chat(
+        context=context,
+        messages=prompt,
+        temperature=0.25,
+        candidate_count=1,
+        top_k=40,
+        top_p=0.95,
+    ).last
+
+    # Simulate response delay
+    time.sleep(1)
+
+    # Display assistant response in chat history
+    st.session_state.messages.append({"role": "teacher", "content": teacher_response})
+    st.text(f'Teacher: {teacher_response}')
